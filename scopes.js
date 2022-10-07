1b86c8e5fc8720ce134607e59f2220e2c035e0fd
harshit71 <harshit.raj@itential.com>
2022-10-07 15:46:46 +0530

    adding scope discussion

---
+++ scopes.js
@@ -0,0 +1,37 @@
+// var x = 100;
+
+// function fun(){
+//     console.log("inside fun 1 = ", x); // 1000
+//     //var x = 1000;
+//     console.log("inside fun 2 = ", x); // 1000
+// }
+
+// fun();
+
+
+// console.log("ouside fun ", x); //100
+
+
+// var teacher = "t1";
+
+// function fun(){
+
+    
+//     console.log(teacher); //undefined
+
+
+//     teacher = "t2";
+
+//     console.log(teacher); // t2
+
+
+// }
+
+// fun();
+
+// console.log(teacher); //t2
+
+//console.log(1 - "123");
+
+// 123NaN
+console.log("123" + {});
